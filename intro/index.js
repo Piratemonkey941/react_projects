9364a05e9a3fb46ddc61f8df93ed9af79783945e
Piratemonkey941 <107444528+Piratemonkey941@users.noreply.github.com>
2023-01-25 14:57:18 -0600

    initial framework notes

---
+++ intro/index.js
@@ -0,0 +1,10 @@
+ReactDOM.render(
+    <h1> Hello Everyone</h1>    
+    , document.getElementById("root"))
+
+// ReactDOM.render(
+//     <ul>
+//     <li>thing one</li>
+//     <li>thing two</li>
+// </ul>    
+//     , document.getElementById("root2"))
---
+++ movies/movies/src/App.js
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import { useEffect } from 'react'
+import './App.css';
+
+
+const App = () => {
+
+}
+
+
+export default App;
