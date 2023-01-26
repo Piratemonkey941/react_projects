481c88b65b03b60cac7e9d660649939a30f7dca2
Piratemonkey941 <107444528+Piratemonkey941@users.noreply.github.com>
2023-01-26 16:08:59 -0600

    navbar implemented

---
+++ youtube_clone/src/componants/Feed.jsx
@@ -0,0 +1,24 @@
+
+import {Box, Stack, Typography} from '@mui/material';
+import { useState } from 'react';
+
+const Feed = () => {
+  return (
+    <Stack
+      sx=
+        {{
+          flexDirection: 
+          {
+            sx: "column",
+            md: "row"
+          }
+        }}
+    >
+
+
+
+    </Stack>
+  )
+}
+
+export default Feed
